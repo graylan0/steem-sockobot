2678fe13e2fc128a3fbfbb66b6b3cd7d3e736832
Jestemkioskiem <Dlephhi@gmail.com>
2018-02-17 23:11:40 +0100

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 until bot.py; do
     echo "'bot.py' crashed with exit code $?. Restarting..." >&2
-    TOKEN=$1 KEY=$2 NAME=$3 python3 ~/Python/steem-sockobot/bot.py
+    python3 ~/Python/steem-sockobot/bot.py
     sleep 1
 done
